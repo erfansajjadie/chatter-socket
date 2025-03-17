6f88177623f807defcc1a23927112f522d572cda
erfan <erfan30101377@gmail.com>
2025-03-17 19:39:58 +0330

    style: format code for better readability in configureSocket function

--- src/helpers/configSocket.ts
+++ src/helpers/configSocket.ts
@@ -34,7 +34,8 @@ function configureSocket(server: http.Server) {
 
     socket.on("sendMessage", async (data) => {
       try {
-        const { userId, conversationId, text, type, file, voiceDuration } = data;
+        const { userId, conversationId, text, type, file, voiceDuration } =
+          data;
 
         // Save the message to the database
         const message = await prisma.message.create({
